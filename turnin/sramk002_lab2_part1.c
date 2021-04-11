74666c5ed05d1d19022e78ed7f095cafa05a5b95
sidrk01 <81598654+sidrk01@users.noreply.github.com>
2021-04-11 14:27:19 -0700

    Update sramk002_lab2_part1.c

--- turnin/sramk002_lab2_part1.c
+++ turnin/sramk002_lab2_part1.c
@@ -17,16 +17,19 @@ int main(void) {
     DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
     DDRB = 0xFF; PORTB = 0x00; //Configure port B's 8 pins as outputs, intializie to 0s
 unsigned char tmpB = 0x00; //Temporary variable to hold the value of B 
-unsigned char tmpA = 0x00; //Temporary variable to hold 
+unsigned char tmpA = 0x00; //Temporary variable to hold A
+unsigned char light = 0x00;
     /* Insert your solution below */
     while (1) {
-    tmpA = PINA; 
-    if (tmpA == 0x01){
-    PORTB = 0x01;
+    tmpA = PINA & 0x01;
+    light = PINA & 0x02;
+    if (tmpA && !(light)){
+    tmpB = 0x01;
 } else {
-    PORTB = 0x00;
+    tmpB = 0x00;
 }
 
+PORTB = tmpB;
     }
-    return 0;
+    return 1;
 }
